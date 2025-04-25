d444b645f32cc863fa06a91fad15c0269887ead8
Nikita Lakkoju <146255672+NikitaLakkoju@users.noreply.github.com>
2025-04-25 15:19:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! I will get back to you soon.');
+  });
+  
